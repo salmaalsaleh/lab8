6e118e529be1b751d03e012b7d5605a644b9e49c
salmaalsaleh <salehsalma06@gmail.com>
2024-11-14 11:18:47 -0500

    Create scriptOne.js

---
+++ scriptOne.js
@@ -0,0 +1,96 @@
+let name = "Salma";{
+    console.log("Name:", name);
+}
+
+let age = 18; {
+console.log("Age:", age);
+}
+
+let myString = "This is question 2"; {
+    console.log("string:", myString);
+}
+
+let myNumber = 18;
+{console.log("number:", myNumber);}
+let myBoolean = false; {
+    console.log("Boolean:", myBoolean);
+}          
+let myUndefined;   {
+    console.log("Undefined:", myUndefined)
+}            
+
+
+let RollNumber = 15;
+if (RollNumber > 10) {
+    console.log("The RollNumber is greater than 10");
+}
+
+
+let isMember = true;
+if (isMember) {
+    console.log("Member discount applied.");
+}
+
+let startValue = 50;
+if (startValue > 0) {
+    console.log("positive");
+} else if (startValue < 0) {
+    console.log("negative");
+} else {
+    console.log("zero");
+}
+
+let a = 6;
+let b = 3;
+
+console.log(a==b)
+console.log(a===b)
+console.log(a!=b)
+console.log(a>b)
+console.log(a<b)
+console.log(a>=b)
+console.log(a<=b)
+
+
+let firstName = "Salma";
+let lastName = "Saleh";
+let age2 = 18;
+console.log("Hello,", firstName, lastName," you are", age, "years old");
+
+var num = 50;
+if (num > 0){
+if (num < 100) 
+    console.log("The number is within range.");
+}
+
+var hasCar = true;
+var hasLicense = true;
+var hasInsurance = true;
+if (true){
+    console.log("You can drive legally.");
+}
+else if (false){
+    console.log("Check your driving eligibility.");
+}
+
+var num = 21;
+if (num %2==0){
+    console.log("The number is even.");
+}
+else{
+    console.log("The number is odd.");
+}
+
+
+var number = 85;
+if (number >= 90){
+    console.log("Grade A.");
+}
+
+else if(number <= 80  <= 89){
+  console.log("Grade B.");
+}
+
+else if(number > 79){
+    console.log("Grade C.");
+}
